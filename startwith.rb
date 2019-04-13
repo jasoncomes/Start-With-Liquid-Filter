85c800f007eeab121860f34eabc5d8ed762a1138
Jason Comes <jcomes@highereducation.com>
2019-04-13 14:05:02 -0500

    Add files via upload

---
+++ startwith.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+# startwith / https://www.notion.so/highereducation/startwith-5017821a08924768b44dd277f61a37cb
+# This will take a given string and add text to the beginning of it. The text will not be added to the given string if the string already begins with the text.
+# {{ string | startwith: string_to_prepend }}
+
+module StartWith
+  def startwith(content, startwidth_content = '')
+    content = content.to_s
+
+    # Return if empty
+    return if (content.nil? || content.empty?) && (startwidth_content.nil? || startwidth_content.empty?)
+
+    # Return results
+    content.start_with?(startwidth_content) ? content : startwidth_content + content
+  end
+end
+
+Liquid::Template.register_filter(StartWith)
